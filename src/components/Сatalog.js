ff109d4ac30906796f5e75ce97a490eb7cd04efa
DanaVoit <bogdana2397@gmail.com>
2024-02-19 14:25:41 +0200

    Updated Catalog

--- src/components/Сatalog.js
+++ src/components/Сatalog.js
@@ -1,16 +1,15 @@
 import catalog from "../components/data/catalog.json";
 import { useState, useEffect } from "react";
+import nav1 from "../assets/img/nav1.png";
 
-export default function Catalog(){
-
-
-  const [filter, setFilter] = useState('all');
+export default function Catalog() {
+  const [filter, setFilter] = useState("all");
 
   const filteredProducts = () => {
-    if (filter === 'all') {
+    if (filter === "all") {
       return catalog;
     } else {
-      return catalog.filter(product => product.category === filter);
+      return catalog.filter((product) => product.category === filter);
     }
   };
 
@@ -19,18 +18,36 @@ export default function Catalog(){
   };
 
   return (
-
-
     <div id="catalog" className="container catalog">
       <div className="catalog-nav">
-        <button onClick={() => handleFilterChange('all')}>Всі</button>
-        <button onClick={() => handleFilterChange('ремені')}>Ремені</button>
-        <button onClick={() => handleFilterChange('гаманці')}>Гаманці</button>
-        <button onClick={() => handleFilterChange('інше')}>Інше</button>
+        <a
+          className={`catalog-nav-item ${filter === "all" ? "active" : ""}`}
+          onClick={() => handleFilterChange("all")}
+        >
+          Всі{" "}
+        </a>
+        <a
+          className={`catalog-nav-item ${filter === "ремені" ? "active" : ""}`}
+          onClick={() => handleFilterChange("ремені")}
+        >
+          Ремені <img src={nav1} width={70} />
+        </a>
+        <a
+          className={`catalog-nav-item ${filter === "гаманці" ? "active" : ""}`}
+          onClick={() => handleFilterChange("гаманці")}
+        >
+          Гаманці
+        </a>
+        <a
+          className={`catalog-nav-item ${filter === "інше" ? "active" : ""}`}
+          onClick={() => handleFilterChange("інше")}
+        >
+          Інше
+        </a>
       </div>
 
       <ul className="catalog-list">
-      {filteredProducts().map(product => (
+        {filteredProducts().map((product) => (
           <li key={product.id} className="catalog-item">
             <div className="catalog-item-image">
               <img src={product.image} alt={product.title} />
@@ -45,4 +62,4 @@ export default function Catalog(){
       </ul>
     </div>
   );
-      }
+}
